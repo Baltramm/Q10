ec78ab96ba827fa35fd751c2b95cf5359e83a5e1
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-06-08 04:10:05 +0600

    Добавьте файлы проекта.

---
+++ Q10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Q10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Writer writer = new Writer();
+
+            ((IWriter)writer).Write();
+
+            Console.ReadKey();
+        }
+    }
+    public interface IWriter
+    {
+        void Write();
+    }
+
+    public class Writer : IWriter
+    {
+        void IWriter.Write()
+        {
+            
+        }
+    }
+}
